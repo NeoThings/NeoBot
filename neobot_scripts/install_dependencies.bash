3d3d5285ef477fe7276ce16561792b8b1b43304c
uplc <new_uplc@163.com>
2025-04-09 22:27:33 +0800

    udpate script

--- neobot_scripts/install_dependencies.bash
+++ neobot_scripts/install_dependencies.bash
@@ -3,11 +3,13 @@ cd ..
 sudo apt install ros-noetic-tf2-sensor-msgs ros-noetic-move-base-msgs
 git clone https://github.com/NeoThings/navigation.git
 git clone https://github.com/NeoThings/neobot_sensors.git
-git clone https://github.com/NeoThings/neobot_manager.git
-git clone https://github.com/NeoThings/neobot_comm.git
 #git clone https://github.com/NeoThings/neobot_models.git
 
 mkdir neobot_kits
 cd neobot_kits
-git clone https://github.com/NeoThings/teleop_tools.git
+git clone https://github.com/NeoThings/teleop_tools.git -b noetic-devel
 git clone https://github.com/NeoThings/teleop_twist_keyboard.git
+
+# manually
+#git clone https://github.com/NeoThings/neobot_manager.git
+#git clone https://github.com/NeoThings/neobot_comm.git
